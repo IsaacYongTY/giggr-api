29fd5bd31251b794bd9a06c730fc0c2f263b6012
Isaac Yong <isaac.yongty@gmail.com>
2021-05-11 09:22:10 +0800

    added jest and tests

---
+++ tests/database-functions.test.js
@@ -0,0 +1,25 @@
+const { addGenresToDatabase, convertKeyToKeyModeInt } = require('../lib/library')
+
+describe("addGenresToDatabase", () => {
+    it("should be working",() => {
+        expect(addGenresToDatabase('pop, metal, funk')).toStrictEqual(['pop', 'metal', 'funk'])
+        expect(addGenresToDatabase('pop')).toStrictEqual(['pop'])
+        expect(addGenresToDatabase('')).toStrictEqual([''])
+        expect(addGenresToDatabase('pop-metal-funk'))
+    })
+    it.todo("")
+})
+
+describe("convertKeyToKeyModeInt", () => {
+    it("should convert key to key and mode", () => {
+        expect(convertKeyToKeyModeInt('C')).toStrictEqual([0,0])
+        expect(convertKeyToKeyModeInt('Am')).toStrictEqual([9,1])
+    })
+
+    it("should return error if the input is invalid",() => {
+        expect(convertKeyToKeyModeInt('')).toStrictEqual(undefined)
+        expect(convertKeyToKeyModeInt('Cmaj7')).toStrictEqual(undefined)
+        expect(convertKeyToKeyModeInt('Cmaj7m')).toStrictEqual(undefined)
+
+    })
+})
