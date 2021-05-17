ff5fc490f366cd4ca258915e414ebd5a56c9e122
Isaac Yong <isaac.yongty@gmail.com>
2021-05-18 02:28:21 +0800

    added app set proxy

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(cookieParser())
+app.set("trust proxy", 1)
 
 app.use('/api/v1/auth', require('./routes/auth.route'))
 app.use('/api/v1/songs', require('./routes/songs.route'))
