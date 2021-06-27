4e582378dc147ba4ec108bd12c7e2f51b9274ee4
Isaac Yong <isaac.yongty@gmail.com>
2021-06-27 17:26:29 +0800

    update authorization

--- middlewares/authChecker.js
+++ middlewares/authChecker.js
@@ -6,13 +6,13 @@ const authChecker = async (req, res, next) => {
 
         const tokenString = req.header("x-auth-token")
         const token = tokenString.split(" ")[1]
-        console.log(token)
         const decoded = jwt.verify(token, process.env.SESSION_SECRET)
 
         req.user = decoded.user
         console.log("auth checker works")
         next()
     } catch (error) {
+        console.log(error)
         res.status(403).send({error})
     }
 
