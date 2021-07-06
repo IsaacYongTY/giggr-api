1fcef0d41df87721c9713a58236ff9e80baa91a4
IsaacYongTY <isaac.yongty@gmail.com>
2021-07-06 12:59:10 +0800

    fix bug

--- src/lib/database-utils/find-or-bulk-create-musicians.ts
+++ src/lib/database-utils/find-or-bulk-create-musicians.ts
@@ -1,7 +1,7 @@
 
 const db = require('../../models')
 
-export default async function findOrBulkCreateMusicians(database: string , musicians: any, user: any) {
+export default async function findOrBulkCreateMusicians(database: string , musicians: any, userId: number) {
 
     if(!musicians || !musicians.length) {
         return []
@@ -9,7 +9,7 @@ export default async function findOrBulkCreateMusicians(database: string , music
 
     return await Promise.all(musicians.map(async (musician : any) =>
         await db[database].models.musician.findOrCreate({
-            defaults: {name: musician, userId: user.id},
+            defaults: {name: musician, userId: userId},
             where: {name: musician}
         })
     ))
